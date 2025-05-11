ca3994c3237a5fa37ce4726556b40adc7ab43682
jasmine xu <jasminexu0812@gmail.com>
2025-05-11 18:11:56 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import About from './pages/About';
 import Projects from './pages/Projects';
 import Footer from './components/Footer'; 
 import Blog from './pages/Blog';
